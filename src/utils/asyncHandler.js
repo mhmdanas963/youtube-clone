22b46b542caacd35954197ffe3b042f9be76cdb8
Mohammed Anas <mohammedanasdk963@gmail.com>
2024-02-24 17:06:02 +0530

    setup router and userController

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,6 +1,6 @@
 // Promise version
 const asyncHandler = (requestHandler) => (req, res, next) => {
-  return new Promise.resolve(requestHandler(req, res, next)).catch((error) =>
-    next(error)
-  );
+  Promise.resolve(requestHandler(req, res, next)).catch((error) => next(error));
 };
+
+export { asyncHandler };
